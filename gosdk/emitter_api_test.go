fb1d2f7eade45e4b08f4eaceee0cf2e874eac474
b00ris <b00ris@mail.ru>
2025-03-27 13:16:35 +0400

    uncomment skip

--- gosdk/emitter_api_test.go
+++ gosdk/emitter_api_test.go
@@ -28,7 +28,6 @@ import (
 )
 
 func TestEmitterCall(t *testing.T) {
-	t.Skip("listener error")
 	dbPath := "./testdb"
 	_ = os.RemoveAll(dbPath)   // Очищаем базу перед тестом
 	defer os.RemoveAll(dbPath) // Очищаем базу после теста
