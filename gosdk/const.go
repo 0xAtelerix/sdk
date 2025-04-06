765058ae8b2ad4864062675c62b3f198db515bec
b00ris <b00ris@mail.ru>
2025-04-06 13:17:55 +0300

    add chainid's

--- gosdk/const.go
+++ gosdk/const.go
@@ -1,7 +1,21 @@
 package gosdk
 
 const (
-	ChainIDEthereum = 2
-	//todo add chainIDs
-	ChainIDSolana = 111
+	EthereumChainID  = 1     // Ethereum Mainnet
+	PolygonChainID   = 137   // Polygon (PoS)
+	BNBChainID       = 56    // BNB Smart Chain (BSC)
+	AvalancheChainID = 43114 // Avalanche C-Chain
+	GnosisChainID    = 100   // Gnosis Chain (xDai)
+	FantomChainID    = 250   // Fantom Opera
+	BaseChainID      = 8453  // Base Mainnet
+
+	//testnets
+	EthereumSepoliaChainID = 11155111 // Ethereum Sepolia Testnet
+	PolygonMumbaiChainID   = 80001    // Polygon Mumbai Testnet
+	BNBTestnetChainID      = 97       // BNB Smart Chain Testnet
+	AvalancheFujiChainID   = 43113    // Avalanche Fuji Testnet
+	GnosisChiadoChainID    = 10200    // Gnosis Chiado Testnet
+	FantomTestnetChainID   = 4002     // Fantom Testnet
+
+	SolanaDevnttChainID = 123231
 )
