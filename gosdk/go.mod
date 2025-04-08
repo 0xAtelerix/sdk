a958faa27f7ba7f4385e59e38367f5bf4d647b8c
b00ris <b00ris@mail.ru>
2025-04-08 19:19:14 +0400

    change encoding to rlp

--- gosdk/go.mod
+++ gosdk/go.mod
@@ -3,12 +3,12 @@ module github.com/0xAtelerix/sdk/gosdk
 go 1.24.1
 
 require (
+	github.com/ethereum/go-ethereum v1.15.7
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/ledgerwatch/erigon-lib v1.0.0
 	github.com/ledgerwatch/log/v3 v3.9.0
 	github.com/rs/zerolog v1.34.0
-	github.com/stretchr/testify v1.8.4
-	github.com/umbracle/ethgo v0.1.3
+	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.5
 	pgregory.net/rapid v1.2.0
@@ -16,28 +16,36 @@ require (
 
 require (
 	github.com/VictoriaMetrics/metrics v1.23.1 // indirect
+	github.com/bits-and-blooms/bitset v1.17.0 // indirect
 	github.com/c2h5oh/datasize v0.0.0-20220606134207-859f65c6625b // indirect
+	github.com/consensys/bavard v0.1.22 // indirect
+	github.com/consensys/gnark-crypto v0.14.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/erigontech/mdbx-go v0.27.14 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/umbracle/fastrlp v0.0.0-20220527094140-59d5dd30e722 // indirect
-	github.com/valyala/fastjson v1.4.1 // indirect
+	github.com/supranational/blst v0.3.14 // indirect
 	github.com/valyala/fastrand v1.1.0 // indirect
 	github.com/valyala/histogram v1.2.0 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/exp v0.0.0-20230711023510-fffb14384f22 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/net v0.36.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
 
 replace github.com/0xAtelerix/sdk/gosdk => .
--- gosdk/multichain.go
+++ gosdk/multichain.go
@@ -4,14 +4,15 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
-	"encoding/json"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+
 	"fmt"
 	"github.com/0xAtelerix/sdk/gosdk/types"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon-lib/kv/mdbx"
 	mdbxlog "github.com/ledgerwatch/log/v3"
 	"github.com/rs/zerolog/log"
-	"github.com/umbracle/ethgo"
 )
 
 const (
@@ -41,31 +42,31 @@ type MultichainStateAccess struct {
 	stateAccessDB map[uint32]kv.RoDB
 }
 
-func (sa *MultichainStateAccess) EthBlock(block types.ExternalBlock) (ethgo.Block, error) {
+func (sa *MultichainStateAccess) EthBlock(block types.ExternalBlock) (*gethtypes.Block, error) {
 	if _, ok := sa.stateAccessDB[uint32(block.ChainID)]; !ok {
-		return ethgo.Block{}, fmt.Errorf("failed to find blockchain db %v", block.ChainID)
+		return nil, fmt.Errorf("failed to find blockchain db %v", block.ChainID)
 	}
 	key := make([]byte, 40)
 	binary.BigEndian.PutUint64(key[:8], block.BlockNumber)
 	copy(key[8:], block.BlockHash[:])
-	ethBlock := ethgo.Block{}
+	ethBlock := gethtypes.Block{}
 	err := sa.stateAccessDB[uint32(block.ChainID)].View(context.TODO(), func(tx kv.Tx) error {
 		v, err := tx.GetOne(EthBlocks, key)
 		if err != nil {
 			return err
 		}
 		//todo fixme rlp or faster encoding
-		return json.Unmarshal(v, &block)
+		return rlp.DecodeBytes(v, &block)
 	})
 	if err != nil {
-		return ethgo.Block{}, err
+		return nil, err
 	}
 	//todo verify block hash
 
-	return ethBlock, nil
+	return &ethBlock, nil
 }
 
-func (sa *MultichainStateAccess) EthReceipts(block types.ExternalBlock) ([]ethgo.Receipt, error) {
+func (sa *MultichainStateAccess) EthReceipts(block types.ExternalBlock) ([]*gethtypes.Receipt, error) {
 	if _, ok := sa.stateAccessDB[uint32(block.ChainID)]; !ok {
 		return nil, fmt.Errorf("failed to find blockchain db %v", block.ChainID)
 	}
@@ -73,7 +74,7 @@ func (sa *MultichainStateAccess) EthReceipts(block types.ExternalBlock) ([]ethgo
 	key := make([]byte, 72)
 	binary.BigEndian.PutUint64(key[:8], block.BlockNumber)
 	copy(key[8:8+32], block.BlockHash[:])
-	blockReceipts := []ethgo.Receipt{}
+	blockReceipts := []*gethtypes.Receipt{}
 	err := sa.stateAccessDB[uint32(block.ChainID)].View(context.TODO(), func(tx kv.Tx) error {
 		c, err := tx.Cursor(EthReceipts)
 		if err != nil {
@@ -81,13 +82,13 @@ func (sa *MultichainStateAccess) EthReceipts(block types.ExternalBlock) ([]ethgo
 		}
 		k, v, err := c.Seek(key)
 		for ; err == nil && len(k) == 72 && bytes.Equal(key[8:8+32], k[8:8+32]); k, v, err = c.Next() {
-			r := ethgo.Receipt{}
+			r := gethtypes.Receipt{}
 			//todo fixme rlp or faster encoding
-			err = json.Unmarshal(v, &r)
+			err = rlp.DecodeBytes(v, &r)
 			if err != nil {
 				return err
 			}
-			blockReceipts = append(blockReceipts, r)
+			blockReceipts = append(blockReceipts, &r)
 		}
 		return nil
 	})
