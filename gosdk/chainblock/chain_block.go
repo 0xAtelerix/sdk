721fcc0aac47726426f4fd490b1215b30a7dfd25
Andrei Ivasko <andrei@fantom.foundation>
2025-10-17 15:31:51 +0300

    chainblock pakckage

--- gosdk/chainblock/chain_block.go
+++ gosdk/chainblock/chain_block.go
@@ -1,70 +1,79 @@
-package block
+package chainblock
 
 import (
 	"fmt"
 	"strconv"
-	
+	"time"
+
+	"github.com/blocto/solana-go-sdk/client"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/fxamacker/cbor/v2"
 
-	"github.com/0xAtelerix/sdk/gosdk/apptypes"
 	"github.com/0xAtelerix/sdk/gosdk"
-	gethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/blocto/solana-go-sdk/client"
-
+	"github.com/0xAtelerix/sdk/gosdk/apptypes"
 )
 
 // ChainBlock augments an AppchainBlock with the ChainID it belongs to.
 type ChainBlock struct {
 	ChainType apptypes.ChainType
-	Block   any
+	Block     any
 }
 
 func NewChainBlock(chainType apptypes.ChainType, blockBytes []byte) *ChainBlock {
-	cb :=  &ChainBlock{}
 	switch chainType {
-	case gosdk.EthereumChainID, gosdk.PolygonChainID, gosdk.BNBChainID, gosdk.GnosisChainID, gosdk.FantomChainID, gosdk.BaseChainID :
+	case gosdk.EthereumChainID,
+		gosdk.PolygonChainID,
+		gosdk.BNBChainID,
+		gosdk.GnosisChainID,
+		gosdk.FantomChainID,
+		gosdk.BaseChainID:
 		var ethBlock gethtypes.Block
 		if err := cbor.Unmarshal(blockBytes, &ethBlock); err != nil {
 			panic(err)
 		}
-		cb = &ChainBlock{
-			ChainType: chainType,
-			Block:     &ethBlock,
-		}
+
+		return &ChainBlock{ChainType: chainType, Block: &ethBlock}
 	case gosdk.SolanaChainID:
 		var solBlock client.Block
 		if err := cbor.Unmarshal(blockBytes, &solBlock); err != nil {
 			panic(err)
 		}
-		cb = &ChainBlock{
-			ChainType: chainType,
-			Block:     &solBlock,
-		}
+
+		return &ChainBlock{ChainType: chainType, Block: &solBlock}
 	default:
 		panic("unsupported chain type")
-
 	}
-	return cb
 }
 
-
-func (cb *ChainBlock) convertToFieldsValues() any {
+func (cb *ChainBlock) convertToFieldsValues() FieldsValues {
 	switch cb.ChainType {
-	case gosdk.EthereumChainID, gosdk.PolygonChainID, gosdk.BNBChainID, gosdk.GnosisChainID, gosdk.FantomChainID, gosdk.BaseChainID :
+	case gosdk.EthereumChainID,
+		gosdk.PolygonChainID,
+		gosdk.BNBChainID,
+		gosdk.GnosisChainID,
+		gosdk.FantomChainID,
+		gosdk.BaseChainID:
 		ethBlock, ok := cb.Block.(*gethtypes.Block)
 		if !ok {
 			panic("invalid block type for EVM chain")
 		}
+
 		return convertEthBlockToFieldsValues(ethBlock)
 	case gosdk.SolanaChainID:
 		solBlock, ok := cb.Block.(*client.Block)
 		if !ok {
 			panic("invalid block type for Solana chain")
 		}
+
 		return convertSolanaBlockToFieldsValues(solBlock)
 	default:
 		panic("unsupported chain type")
 	}
+}
+
+type FieldsValues struct {
+	Fields []string
+	Values []string
 }
 
 func convertEthBlockToFieldsValues(block *gethtypes.Block) FieldsValues {
@@ -92,20 +101,20 @@ func convertEthBlockToFieldsValues(block *gethtypes.Block) FieldsValues {
 		block.Number().String(),
 		block.Hash().Hex(),
 		block.ParentHash().Hex(),
-		block.Nonce().String(),
+		fmt.Sprintf("0x%x", block.Nonce()),
 		block.UncleHash().Hex(),
-		block.Bloom().String(),
+		fmt.Sprintf("0x%x", block.Bloom().Bytes()),
 		block.TxHash().Hex(),
 		block.Root().Hex(),
 		block.ReceiptHash().Hex(),
 		block.Coinbase().Hex(),
 		block.Difficulty().String(),
-		block.Difficulty().String(), // TotalDifficulty is not directly available
+		block.Difficulty().String(), // approximate total difficulty
 		fmt.Sprintf("0x%x", block.Extra()),
-		strconv.FormatUint(block.Size().Uint64(), 10),
+		strconv.FormatUint(block.Size(), 10),
 		strconv.FormatUint(block.GasLimit(), 10),
 		strconv.FormatUint(block.GasUsed(), 10),
-		strconv.FormatUint(block.Time().Uint64(), 10),
+		strconv.FormatUint(block.Time(), 10),
 	}
 
 	return FieldsValues{Fields: fields, Values: values}
@@ -127,8 +136,16 @@ func convertSolanaBlockToFieldsValues(block *client.Block) FieldsValues {
 		strconv.FormatUint(block.ParentSlot, 10),
 		strconv.Itoa(len(block.Transactions)),
 		strconv.Itoa(len(block.Rewards)),
-		strconv.FormatInt(block.BlockTime, 10),
+		formatBlockTime(block.BlockTime),
 	}
 
 	return FieldsValues{Fields: fields, Values: values}
+}
+
+func formatBlockTime(t *time.Time) string {
+	if t == nil {
+		return "0"
+	}
+
+	return strconv.FormatInt(t.Unix(), 10)
 }
---
+++ gosdk/chainblock/chain_block_test.go
@@ -0,0 +1,139 @@
+package chainblock
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/blocto/solana-go-sdk/client"
+	"github.com/ethereum/go-ethereum/common"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/fxamacker/cbor/v2"
+	"github.com/stretchr/testify/require"
+
+	"github.com/0xAtelerix/sdk/gosdk"
+	"github.com/0xAtelerix/sdk/gosdk/apptypes"
+)
+
+func TestConvertEthBlockToFieldsValues(t *testing.T) {
+	header := &gethtypes.Header{
+		Number:      big.NewInt(42),
+		ParentHash:  common.HexToHash("0x01"),
+		Nonce:       gethtypes.BlockNonce{0xaa},
+		UncleHash:   gethtypes.EmptyUncleHash,
+		Bloom:       gethtypes.Bloom{0x01},
+		TxHash:      common.Hash{},
+		Root:        common.Hash{},
+		ReceiptHash: common.Hash{},
+		Coinbase:    common.HexToAddress("0x000000000000000000000000000000000000c0de"),
+		Difficulty:  big.NewInt(123456),
+		Extra:       []byte{0xde, 0xad},
+		GasLimit:    1000000,
+		GasUsed:     500000,
+		Time:        1700000000,
+	}
+	block := gethtypes.NewBlock(header, nil, nil, nil)
+
+	fv := convertEthBlockToFieldsValues(block)
+	require.Equal(t, "42", fv.Values[0])
+	require.Equal(t, block.Hash().Hex(), fv.Values[1])
+	require.Equal(t, fmt.Sprintf("0x%x", block.Nonce()), fv.Values[3])
+	require.Equal(t, strconv.FormatUint(block.GasLimit(), 10), fv.Values[14])
+}
+
+func TestConvertSolanaBlockToFieldsValues(t *testing.T) {
+	blockTime := time.Unix(1700000000, 0)
+	sol := &client.Block{
+		Blockhash:         "hash",
+		PreviousBlockhash: "prev",
+		ParentSlot:        77,
+		Transactions:      make([]client.BlockTransaction, 3),
+		Rewards:           make([]client.Reward, 2),
+		BlockTime:         &blockTime,
+	}
+
+	fv := convertSolanaBlockToFieldsValues(sol)
+	require.Equal(t, "hash", fv.Values[0])
+	require.Equal(t, "3", fv.Values[3])
+	require.Equal(t, strconv.FormatInt(blockTime.Unix(), 10), fv.Values[5])
+}
+
+func TestChainBlockConvertToFieldsValues_Eth(t *testing.T) {
+	header := &gethtypes.Header{
+		Number: big.NewInt(1),
+	}
+	block := gethtypes.NewBlock(header, nil, nil, nil)
+	cb := &ChainBlock{
+		ChainType: gosdk.EthereumChainID,
+		Block:     block,
+	}
+
+	fv := cb.convertToFieldsValues()
+	require.Equal(t, block.Number().String(), fv.Values[0])
+}
+
+func TestChainBlockConvertToFieldsValues_Solana(t *testing.T) {
+	sol := &client.Block{
+		Blockhash: "hash",
+	}
+	cb := &ChainBlock{
+		ChainType: gosdk.SolanaChainID,
+		Block:     sol,
+	}
+
+	fv := cb.convertToFieldsValues()
+	require.Equal(t, "hash", fv.Values[0])
+}
+
+func TestNewChainBlock_Ethereum(t *testing.T) {
+	header := &gethtypes.Header{
+		Number: big.NewInt(100),
+	}
+	blk := gethtypes.NewBlock(header, nil, nil, nil)
+	encoded, err := cbor.Marshal(blk)
+	require.NoError(t, err)
+
+	var cb *ChainBlock
+
+	require.NotPanics(t, func() {
+		cb = NewChainBlock(gosdk.EthereumChainID, encoded)
+	})
+
+	require.Equal(t, gosdk.EthereumChainID, cb.ChainType)
+	_, ok := cb.Block.(*gethtypes.Block)
+	require.True(t, ok)
+}
+
+func TestNewChainBlock_Solana(t *testing.T) {
+	blockHeight := int64(55)
+	blockTime := time.Unix(1700000000, 0)
+	sol := client.Block{
+		Blockhash:         "sol-hash",
+		BlockHeight:       &blockHeight,
+		BlockTime:         &blockTime,
+		PreviousBlockhash: "prev",
+	}
+	encoded, err := cbor.Marshal(sol)
+	require.NoError(t, err)
+
+	var cb *ChainBlock
+
+	require.NotPanics(t, func() {
+		cb = NewChainBlock(gosdk.SolanaChainID, encoded)
+	})
+
+	require.Equal(t, gosdk.SolanaChainID, cb.ChainType)
+	solBlock, ok := cb.Block.(*client.Block)
+	require.True(t, ok)
+	require.Equal(t, sol.Blockhash, solBlock.Blockhash)
+	require.NotNil(t, solBlock.BlockTime)
+	require.Equal(t, blockTime.Unix(), solBlock.BlockTime.Unix())
+}
+
+func TestNewChainBlock_UnsupportedChainPanics(t *testing.T) {
+	require.Panics(t, func() {
+		NewChainBlock(apptypes.ChainType(999999), []byte{})
+	})
+}
