b32b44a932d2c78b6497e3f36d7c881e32ba96b9
Andrei Ivasko <andrei@fantom.foundation>
2025-10-17 14:50:04 +0300

    WIP ChainBlock

--- gosdk/block/chain_block.go
+++ gosdk/block/chain_block.go
@@ -1,118 +1,46 @@
 package block
 
 import (
-	"crypto/sha256"
-	"encoding/binary"
-
+	
 	"github.com/fxamacker/cbor/v2"
 
 	"github.com/0xAtelerix/sdk/gosdk/apptypes"
+	"github.com/0xAtelerix/sdk/gosdk"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/blocto/solana-go-sdk/client"
+
 )
 
 // ChainBlock augments an AppchainBlock with the ChainID it belongs to.
 type ChainBlock struct {
-	ChainID uint64
-	Block   apptypes.AppchainBlock // or map[apptypes.ChainType]apptypes.AppchainBlock
+	ChainType apptypes.ChainType
+	Block   any
 }
 
-// type ChainBlock struct {
-// 	ChainID uint64
-// 	Block   any
-// }
+func NewChainBlock(chainType apptypes.ChainType, blockBytes []byte) *ChainBlock {
+	cb :=  &ChainBlock{}
+	switch chainType {
+	case gosdk.EthereumChainID, gosdk.PolygonChainID, gosdk.BNBChainID, gosdk.GnosisChainID, gosdk.FantomChainID, gosdk.BaseChainID :
+		var ethBlock gethtypes.Block
+		if err := cbor.Unmarshal(blockBytes, &ethBlock); err != nil {
+			panic(err)
+		}
+		cb = &ChainBlock{
+			ChainType: chainType,
+			Block:     &ethBlock,
+		}
+	case gosdk.SolanaChainID:
+		var solBlock client.Block
+		if err := cbor.Unmarshal(blockBytes, &solBlock); err != nil {
+			panic(err)
+		}
+		cb = &ChainBlock{
+			ChainType: chainType,
+			Block:     &solBlock,
+		}
+	default:
+		panic("unsupported chain type")
 
-
-// type ChainBlock struct {
-// 	map[uint64]any  // chainID -> inner block
-// }
-
-
-/*
-Now we have 2 types of inner blocks: EthereumBlock and SolanaBlock.
-
-type EthereumBlock struct {
-	Header gethtypes.Header
-	Body   gethtypes.Body
-}
-
-type Block struct {
-	Blockhash         string
-	BlockTime         *time.Time
-	BlockHeight       *int64
-	PreviousBlockhash string
-	ParentSlot        uint64
-	Transactions      []BlockTransaction
-	Signatures        []string
-	Rewards           []Reward
-}
-
-
-*/
-
-
-var _ apptypes.AppchainBlock = (*ChainBlock)(nil)
-
-// Number returns the inner block number or zero if the block is nil.
-func (cb *ChainBlock) Number() uint64 {
-	if cb == nil || cb.Block == nil {
-		return 0
 	}
-
-	return cb.Block.Number()
-}
-
-// Hash returns the inner block hash or the zero hash if the block is nil.
-func (cb *ChainBlock) Hash() [32]byte {
-	if cb == nil || cb.Block == nil {
-		return [32]byte{}
-	}
-
-	var chainBuf [8]byte
-	binary.BigEndian.PutUint64(chainBuf[:], cb.ChainID)
-
-	chainHash := sha256.Sum256(chainBuf[:])
-	blockHash := cb.Block.Hash()
-
-	hasher := sha256.New()
-	hasher.Write(chainHash[:])
-	
-	hasher.Write(blockHash[:])
-
-	var out [32]byte
-	copy(out[:], hasher.Sum(nil))
-
-	return out
-}
-
-// StateRoot returns the inner block state root or zero if the block is nil.
-func (cb *ChainBlock) StateRoot() [32]byte {
-	if cb == nil || cb.Block == nil {
-		return [32]byte{}
-	}
-
-	return cb.Block.StateRoot()
-}
-
-type chainBlockPayload struct {
-	ChainID uint64 `cbor:"chainId"`
-	Block   []byte `cbor:"block"`
-}
-
-// Bytes encodes the chain-id alongside the inner block bytes. If the block
-// is nil it returns nil.
-func (cb *ChainBlock) Bytes() []byte {
-	if cb == nil || cb.Block == nil {
-		return nil
-	}
-
-	payload := chainBlockPayload{
-		ChainID: cb.ChainID,
-		Block:   cb.Block.Bytes(),
-	}
-
-	data, err := cbor.Marshal(payload)
-	if err != nil {
-		return nil
-	}
-
-	return data
+	return cb
 }
